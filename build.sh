34c26157c1dab08dea08ab99b605d769db52a98d
MrCelsius88 <bulletman27@outlook.com>
2022-10-29 15:00:08 -0400

    Changed to GCC

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 code="$PWD"
 opts=-g
-linker_opts="-lglfw3 -lGL -lX11 -lpthread -lXrandr -lXi -ldl"
+linker_opts="-lglfw3 -lGL -lX11 -lpthread -lXrandr -lXi -ldl -lm"
 cd build > /dev/null
-g++ $opts $code/src/main.c $linker_opts -o DungeonMaster
+gcc $opts $code/src/main.c $linker_opts -o DungeonMaster
 cd $code > /dev/null
